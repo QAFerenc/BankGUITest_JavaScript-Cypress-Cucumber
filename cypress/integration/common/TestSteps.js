316df3303778e2646ef2bfe254cc82757888ef07
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-04-12 00:18:32 +0200

    Update TestSteps.js

--- cypress/integration/common/TestSteps.js
+++ cypress/integration/common/TestSteps.js
@@ -9,14 +9,12 @@ Before( function()
 {
     // importing the locators from the .json file may not be a good idea, as they are not Test Data.
     // Test Data are in the .feature files
-    // Using locators from a fixture file is just an example of using the fixture fiile
+    // Using locators from a fixture file is just an example of using the fixture file
     cy.fixture('element_dom_mapping').then(function (element_dom_mapping)  {
     cy.element_dom_mapping = element_dom_mapping
 })})
 
-
-Given('A webpage as {string}', (page) => {
-    
+Given('A webpage as {string}', (page) => {   
     cy.visit(page)
  })
 
@@ -33,7 +31,6 @@ And('Customer fills element as {string} with name as {string} with text as {stri
 // Currency type can be "CZK" or "EUR"
 Then('Transaction history in Currency as {string} and value_1 as {string} value_2 as {string}',(currency, value_1,value_2)=>{
     mainScreen.readAmounts(currency, value_1,value_2)
-
 })
 
 Then('Customer should see text as {string}',(text)=> {
